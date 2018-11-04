03cfb8fb95217edf2d4694126b0f46646a2d01e2
Brian Foster <brriann@gmail.com>
2018-11-04 10:01:21 -0700

    Test files, Test JUnit class, start to implement MovieDB funcs

--- src/MovieDB/MovieDatabase.java
+++ src/MovieDB/MovieDatabase.java
@@ -19,15 +19,49 @@ public class MovieDatabase {
 	}
 	
 	public void addMovie(String name, String[] actors) {
-		
+		for (Movie movie : movieList) {
+			if (movie.getName().equals(name)) {
+				return;
+			}
+		}
+		ArrayList<String> newActors = new ArrayList<String>();
+		ArrayList<Actor> existingActors = new ArrayList<Actor>();
+		for (String actor : actors) {
+			int counter = getActorList().size();
+			for (Actor dbActor : getActorList()) {
+				if (dbActor.getName().equals(actor)) {
+					existingActors.add(dbActor);
+				}
+				counter--;
+				if (counter == 0) {
+					newActors.add(actor);
+				}
+			}
+		}
 	}
 	
 	public void addRating(String name, double rating) {
-		
+		Movie movieToRate = null;
+		for (Movie movie : movieList) {
+			if (movie.getName().equals(name)) {
+				movieToRate = movie;
+			}
+		}
+		if (movieToRate != null) {
+			movieToRate.setRating(rating);
+		}
 	}
 	
 	public void updateRating(String name, double newRating) {
-		
+		Movie movieToRate = null;
+		for (Movie movie : movieList) {
+			if (movie.getName().equals(name)) {
+				movieToRate = movie;
+			}
+		}
+		if (movieToRate != null) {
+			movieToRate.setRating(newRating);
+		}
 	}
 	
 	public String getBestActor() {
---
+++ src/MovieDB/MovieDatabaseTest.java
@@ -0,0 +1,39 @@
+package MovieDB;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MovieDatabaseTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testAddMovie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddRating() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testUpdateRating() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetBestActor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetBestMovie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
